c23971241a47cee1770bd2d5193f5669fc11c827
younes2808 <benhaida.mehdi@gmail.com>
2025-01-17 14:48:20 +0100

    new features and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
 import { useState, useEffect } from 'react';
 import Globe from 'react-globe.gl';
-import globeImage from './assets/earth-dark.jpg';
 import './App.css';
 
 function App() {
-  const myData = [
-    {
-      lat: 29.953204744601763,
-      lng: -90.08925929478903,
-      altitude: 0.4,
-      color: '#00ff33',
-    },
-    {
-      lat: 28.621322361013092,
-      lng: 77.20347613099612,
-      altitude: 0.4,
-      color: '#ff0000',
-    },
-    {
-      lat: -43.1571459086602,
-      lng: 172.72338919659848,
-      altitude: 0.4,
-      color: '#ffff00',
-    },
-  ];
-
+  //const myData = [
   // Handle resizing and maintaining the size of the globe
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
@@ -42,10 +21,7 @@ function App() {
   return (
     <div className="cursor-move">
       <Globe
-        globeImageUrl={globeImage}
-        pointsData={myData}
-        pointAltitude="altitude"
-        pointColor="color"
+      globeTileEngineUrl={(x, y, l) => `https://tile.openstreetmap.org/${l}/${x}/${y}.png`}
         width={width}  // Set the width dynamically
         height={height} // Set the height dynamically
       />
---
+++ src/api.js
@@ -0,0 +1,40 @@
+import WebSocket from "ws";
+
+const socket = new WebSocket("wss://stream.aisstream.io/v0/stream");
+
+// Replace process.env.AISSTREAM_API_KEY with your actual API key
+const API_KEY = "9ae7ab0f44f03c778ea82454b5887ae3c115c14f";
+
+socket.addEventListener("open", () => {
+  const subscriptionMessage = {
+    APIkey: API_KEY,
+    BoundingBoxes: [
+    // Atlantic Ocean
+    [[-80, -40], [-20, 40]],
+    // Pacific Ocean (North)
+    [[120, -10], [180, 60]],
+    // Pacific Ocean (South)
+    [[-180, -60], [-80, 0]],
+    // Indian Ocean
+    [[20, -40], [120, 30]],
+    // Southern Ocean
+    [[-180, -90], [180, -60]],
+    ],
+  };
+  console.log(JSON.stringify(subscriptionMessage));
+  socket.send(JSON.stringify(subscriptionMessage));
+});
+
+socket.addEventListener("error", (event) => {
+  console.log(event);
+});
+
+socket.addEventListener("message", (event) => {
+  let aisMessage = JSON.parse(event.data);
+  if (aisMessage["MessageType"] === "PositionReport") {
+    let positionReport = aisMessage["Message"]["PositionReport"];
+    console.log(
+      `ShipId: ${positionReport["UserID"]} Latitude: ${positionReport["Latitude"]} Longitude: ${positionReport["Longitude"]}`
+    );
+  }
+});
